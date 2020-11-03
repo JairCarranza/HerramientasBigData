c667f298dee8f242328bb5d745883c2823ed0d47
Jair Guillermo Carranza Moreno <jcarranza@ifxcorp.com>
2020-11-03 16:05:53 -0500

    Creacion conexión con Mongo y metodos de consulta de todos los clientes y pedidos

---
+++ NoSQL/Access/ClientDb.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoSQL.Access
+{
+    class ClientDb
+    {
+        public static MongoClient AccessDb()
+        {
+            string connectionString = Constants.MINOS_MONGO_DB;
+
+            MongoClientSettings settings = MongoClientSettings.FromUrl(
+                new MongoUrl(connectionString)
+            );
+            settings.SslSettings =
+                new SslSettings { EnabledSslProtocols = SslProtocols.Tls12 };
+
+            MongoClient client = new MongoClient(settings);
+
+            return client;
+        }
+    }
+}
